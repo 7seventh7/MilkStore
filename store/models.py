eb338e3f9735e1aae873ecc9a384b7ba0abc0a6e
7seventh7 <m.semerukhin@gmail.com>
2022-12-24 21:40:40 +0200

    maintain admin panal

--- store/models.py
+++ store/models.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Product(models.Model):
-    title = models.CharField(max_length=120, null=True)
-    content = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    title = models.CharField(max_length=120, null=True, help_text="Введите название продукта")
+    content = models.TextField(blank=True, null=True, help_text="Введите описание продукта")
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99, help_text="Введите цену продукта")
 
+    class Meta:
+        verbose_name = ('Продукт')
+        verbose_name_plural = ('Продукты')
     @property
     def sale_price(self):
         return "%.2f" %(float(self.price) * 0.8)
@@ -16,7 +19,18 @@ class Product(models.Model):
 class Purchase(models.Model):
     product_list = models.ManyToManyField(Product, related_name='product_list')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    class Meta:
+        verbose_name = ('Покупка')
+        verbose_name_plural = ('Покупки')
+
 
 class Transaction(models.Model):
     time_creation = models.DateTimeField(auto_now_add=True)
-    purchase = models.OneToOneField(Purchase, related_name='transaction', on_delete=models.PROTECT)
+    purchase = models.OneToOneField(Purchase, related_name='transaction', on_delete=models.PROTECT, verbose_name='Покупка')
+    class Meta:
+        verbose_name = ('Чек')
+        verbose_name_plural = ('Чеки')
+
+    def __str__(self):
+        return f'Покупка: {self.pk}'
+
